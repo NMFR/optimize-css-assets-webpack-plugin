5539acee41a70bf4518175d8a760bd7ccd29f866
Nuno Rodrigues <nunomr@gmail.com>
2017-05-19 10:14:25 +0100

    Fix promises handling
    Fixes #8

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ var webpackSources = require('webpack-sources');
 
 function OptimizeCssAssetsPlugin(options) {
   this.options = options || {};
-  
+
   if (this.options.assetNameRegExp === undefined) {
     this.options.assetNameRegExp = /\.css$/g;
   }
-  
+
   if (this.options.cssProcessor === undefined) {
     this.options.cssProcessor = require('cssnano');
   }
-  
+
   if (this.options.cssProcessorOptions === undefined) {
     this.options.cssProcessorOptions = {};
   }
-  
+
   if (this.options.canPrint === undefined) {
     this.options.canPrint = true;
   }
@@ -23,12 +23,12 @@ function OptimizeCssAssetsPlugin(options) {
 
 OptimizeCssAssetsPlugin.prototype.print = function() {
   if (this.options.canPrint) {
-    console.log.apply(console, arguments);  
+    console.log.apply(console, arguments);
   }
 };
 
 OptimizeCssAssetsPlugin.prototype.processCss = function(css, assetName) {
-  return this.options.cssProcessor.process(css, Object.assign({ to: assetName }, this.options.cssProcessorOptions)); 
+  return this.options.cssProcessor.process(css, Object.assign({ to: assetName }, this.options.cssProcessorOptions));
 };
 
 OptimizeCssAssetsPlugin.prototype.createCssAsset = function(css, originalAsset) {
@@ -36,63 +36,66 @@ OptimizeCssAssetsPlugin.prototype.createCssAsset = function(css, originalAsset) 
 };
 
 OptimizeCssAssetsPlugin.prototype.apply = function(compiler) {
-  
+
   var self = this;
-  
+
   compiler.plugin('emit', function(compilation, compileCallback) {
-    
+
     self.print('\nStarting to optimize CSS...');
-    
+
     var assets = compilation.assets;
-    
+
     var cssAssetNames = _.filter(
-      _.keys(assets), 
-      function(assetName) { 
+      _.keys(assets),
+      function(assetName) {
         return assetName.match(self.options.assetNameRegExp);
       }
     );
-    
+
     var hasErrors = false;
     var promises = [];
-    
+
     _.each(
       cssAssetNames,
       function(assetName) {
-        
+
         self.print('Processing ' + assetName + '...');
-        
+
         var asset = assets[assetName];
-        
+
         var originalCss = asset.source();
-        
-        var promise = self.processCss(originalCss, assetName); 
-        
-        promise.then(
-          function (result) {
-            
-            if (hasErrors) {
-              self.print('Skiping ' + assetName + ' because of an error.');  
-              return;
+
+        var promise = self
+          .processCss(originalCss, assetName)
+          .then(
+            function (result) {
+
+              if (hasErrors) {
+                self.print('Skiping ' + assetName + ' because of an error.');
+                return;
+              }
+
+              var processedCss = result.css;
+
+              assets[assetName] = self.createCssAsset(processedCss, asset);
+
+              self.print('Processed ' + assetName + ', before: ' + originalCss.length + ', after: ' + processedCss.length + ', ratio: ' + (Math.round(((processedCss.length * 100) / originalCss.length) * 100) / 100) + '%');
+
             }
-            
-            var processedCss = result.css;
-            
-            assets[assetName] = self.createCssAsset(processedCss, asset);
-            
-            self.print('Processed ' + assetName + ', before: ' + originalCss.length + ', after: ' + processedCss.length + ', ratio: ' + (Math.round(((processedCss.length * 100) / originalCss.length) * 100) / 100) + '%');
-            
-          }, function(err) {
-            hasErrors = true;
-            self.print('Error processing file: ' + assetName);
-            console.error(err);
-          }
-        );
-        
+          ).catch(function(err) {
+              hasErrors = true;
+              self.print('Error processing file: ' + assetName);
+              throw err;
+            }
+          );
+
         promises.push(promise);
       }
     );
-    
-    Promise.all(promises).then(function () { compileCallback(); }, compileCallback);
+
+    Promise.all(promises)
+      .then(function () { compileCallback(); })
+      .catch(compileCallback);
   });
 };
 
