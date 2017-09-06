56931ef8e2b6cebe0910f502fae08265e96ba70f
Fareed Namrouti <digital.flowers@hotmail.com>
2017-09-07 01:34:13 +0900

    making it work with webpack compression plugin (#18)
    Support webpack compression plugin
    
    webpack compression plugin use the phase 'compilation.optimize-assets' so we should use the phase 'compilation.optimize-chunk-assets'
    
    * using LastCallWebpackPlugin.PHASE.OPTIMIZE_CHUNK_ASSETS
    
    * using LastCallWebpackPlugin.prototype.PHASE.OPTIMIZE_CHUNK_ASSETS

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ function OptimizeCssAssetsPlugin(options) {
   this.lastCallInstance = new LastCallWebpackPlugin({
     assetProcessors: [
       {
+        phase: LastCallWebpackPlugin.prototype.PHASE.OPTIMIZE_CHUNK_ASSETS,
         regExp: this.options.assetNameRegExp,
         processor: function (assetName, asset, assets) {
           return self.processCss(assetName, asset, assets);
