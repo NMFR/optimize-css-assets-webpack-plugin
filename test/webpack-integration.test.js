3c2a008ed9259e13bb76b9fea4c8ec312aa4dd9c
Scott Norvell <scottlnorvell@gmail.com>
2018-02-01 10:53:34 -0500

    Add some webpack integration tests
    NOTE: I copied/modified boilerplate from
    
    https://github.com/webpack-contrib/extract-text-webpack-plugin

---
+++ test/webpack-integration.test.js
@@ -0,0 +1,48 @@
+/* eslint-disable import/no-dynamic-require, global-require */
+import fs from 'fs';
+import path from 'path';
+import webpack from 'webpack';
+
+const cases = process.env.CASES ? process.env.CASES.split(',') : fs.readdirSync(path.join(__dirname, 'cases'));
+
+describe('Webpack Integration Tests', () => {
+  cases.forEach((testCase) => {
+    it(testCase, (done) => {
+      const testDirectory = path.join(__dirname, 'cases', testCase);
+      const outputDirectory = path.join(__dirname, 'js', testCase);
+      const expectedDirectory = path.join(testDirectory, 'expected');
+
+      const configFile = path.join(testDirectory, 'webpack.config.js');
+      const config = Object.assign(
+        fs.existsSync(configFile) ? require(configFile) : { entry: { test: './index.js' } },
+        {
+          context: testDirectory,
+          output: {
+            filename: '[name].js',
+            path: outputDirectory
+          }
+        }
+      );
+
+      webpack(config, (err, stats) => {
+        if (err) return done(err);
+        if (stats.hasErrors()) return done(new Error(stats.toString()));
+        fs.readdirSync(expectedDirectory).forEach((file) => {
+          const expectedFile = readFileOrEmpty(path.join(expectedDirectory, file));
+          const actualFile = readFileOrEmpty(path.join(outputDirectory, file));
+          expect(actualFile).toEqual(expectedFile);
+          expect(actualFile).toMatchSnapshot();
+        });
+        done();
+      });
+    });
+  });
+});
+
+function readFileOrEmpty(path) {
+  try {
+    return fs.readFileSync(path, 'utf-8');
+  } catch (e) {
+    return '';
+  }
+}
